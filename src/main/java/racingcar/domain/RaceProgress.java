4c2ae96a39cacacf6b7f384edd092d6514a47d61
2jaeheon <jaeheon0826@jbnu.ac.kr>
2025-12-27 12:08:38 +0900

    refactor: race progress
    Signed-off-by: 2jaeheon <jaeheon0826@jbnu.ac.kr>

--- src/main/java/racingcar/domain/RaceProgress.java
+++ src/main/java/racingcar/domain/RaceProgress.java
@@ -3,11 +3,12 @@ package racingcar.domain;
 import java.util.List;
 
 public record RaceProgress(List<CarStatus> cars) {
-    // 불변 리스트를 고정
+    // validate를 수행
     public RaceProgress {
         if (cars == null) {
             throw new IllegalArgumentException("진행 상태 목록은 null일 수 없습니다.");
         }
+        // 불변 리스트를 고정
         cars = List.copyOf(cars);
     }
 }
